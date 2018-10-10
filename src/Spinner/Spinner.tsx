e7c22ca59f0ed6c0e322beea4740786fe0c8fa61
Xiaofan Wu <xiaofan2406@gmail.com>
2018-10-10 21:08:44 +1100

    add color prop to Spinner

--- src/Spinner/Spinner.tsx
+++ src/Spinner/Spinner.tsx
@@ -8,12 +8,22 @@ export interface SpinnerProps extends React.HTMLAttributes<HTMLDivElement> {
    */
   size?: number;
   /**
-   * The thickness scale of the spinner.
+   * The thickness scale for the spinner.
    */
   scale?: number;
+  /**
+   * The color for the spinner.
+   */
+  color?: string;
 }
 
-const Spinner: SFC<SpinnerProps> = ({ size, scale, className, ...rest }) => {
+const Spinner: SFC<SpinnerProps> = ({
+  size,
+  scale,
+  color,
+  className,
+  ...rest
+}) => {
   const maxBorder = size! / 2;
   const scaledBorder = (size! * scale!) / 10;
   const borderSize = scaledBorder >= maxBorder ? maxBorder : scaledBorder;
@@ -32,7 +42,7 @@ const Spinner: SFC<SpinnerProps> = ({ size, scale, className, ...rest }) => {
         className={css`
           width: ${size!}px;
           height: ${size!}px;
-          border: ${borderSize}px solid ${theme.primaryColor};
+          border: ${borderSize}px solid ${color};
           border-bottom-color: transparent;
           border-radius: 100%;
           background: transparent;
@@ -46,6 +56,7 @@ const Spinner: SFC<SpinnerProps> = ({ size, scale, className, ...rest }) => {
 Spinner.defaultProps = {
   size: 60,
   scale: 1,
+  color: theme.primaryColor,
 };
 
 export default Spinner;
