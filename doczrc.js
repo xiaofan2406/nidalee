1e2fb6994368712e907a84f68694e1c6afc8540d
Xiaofan Wu <xiaofan2406@gmail.com>
2018-10-15 21:32:51 +1100

    remove docs dist files

--- doczrc.js
+++ doczrc.js
@@ -1,13 +1,9 @@
 import pkg from './package.json';
 
-const libName = pkg.name;
-
 export default {
   typescript: true,
-  indexHtml: './docs-index.html',
-  dest: './docs',
-  hashRouter: true,
-  base: `/${libName}/`,
+  indexHtml: './docs/index.html',
+  dest: './dist/docs',
   ordering: 'ascending',
   description: pkg.description,
 };
