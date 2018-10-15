7579349d65a7f19f5a32c736e6875c1cdca00433
Xiaofan Wu <xiaofan2406@gmail.com>
2018-10-15 21:25:04 +1100

    fix up docz setup for github pages

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,13 @@
-module.exports = {
+import pkg from './package.json';
+
+const libName = pkg.name;
+
+export default {
   typescript: true,
   indexHtml: './docs-index.html',
-  dest: '/docs',
+  dest: './docs',
+  hashRouter: true,
+  base: `/${libName}/`,
+  ordering: 'ascending',
+  description: pkg.description,
 };
