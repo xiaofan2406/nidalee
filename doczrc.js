9947435780fdc3b4b0935e0d6aacedec0a44abcf
Xiaofan Wu <xiaofan2406@gmail.com>
2019-02-23 12:28:16 +1100

    fix dev setup

--- doczrc.js
+++ doczrc.js
@@ -1,13 +1,16 @@
 import ip from 'ip';
 import pkg from './package.json';
 
+const host = ip.address();
+
 export default {
   typescript: true,
   indexHtml: './docs/index.html',
   dest: './dist/docs',
   ordering: 'ascending',
   description: pkg.description,
-  host: ip.address(),
+  host,
+  websocketHost: host,
   modifyBabelRc: config => {
     config.presets.push('@emotion/babel-preset-css-prop');
 
