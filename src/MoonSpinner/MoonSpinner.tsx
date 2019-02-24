b712718f4a6ce338a664b7a86caf930702ec4dba
Xiaofan Wu <xiaofan2406@gmail.com>
2019-02-24 13:56:46 +1100

    add temp MoonSpinner

---
+++ src/MoonSpinner/MoonSpinner.tsx
@@ -0,0 +1,61 @@
+import React, { FunctionComponent } from 'react';
+import { css } from '@emotion/core';
+import { theme, spinAnimation } from '../styles';
+
+interface MoonSpinnerProps {
+  size?: number;
+  color?: string;
+}
+
+const MoonSpinner: FunctionComponent<MoonSpinnerProps> = ({ size, color }) => {
+  const sizeValue = size!;
+  const speed = 0.85;
+
+  return (
+    <div
+      css={css`
+        display: inline-block;
+      `}
+    >
+      <div
+        css={css`
+          animation: ${spinAnimation} ${speed}s 0s infinite linear;
+          animation-fill-mode: forwards;
+          position: relative;
+        `}
+      >
+        <div
+          css={css`
+            width: ${sizeValue / 10}px;
+            height: ${sizeValue / 10}px;
+            border-radius: 100%;
+
+            animation: ${spinAnimation} ${speed}s 0s infinite linear;
+            animation-fill-mode: forwards;
+
+            background-color: ${color};
+            opacity: 0.8;
+            position: absolute;
+            top: ${sizeValue / 2 - sizeValue / 20}px;
+          `}
+        />
+        <div
+          css={css`
+            width: ${sizeValue}px;
+            height: ${sizeValue}px;
+            border-radius: 100%;
+            border: ${sizeValue / 10}px solid ${color};
+            opacity: 0.1;
+          `}
+        />
+      </div>
+    </div>
+  );
+};
+
+MoonSpinner.defaultProps = {
+  size: 60,
+  color: theme.primaryColor,
+};
+
+export default MoonSpinner;
