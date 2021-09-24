0dbf57bb92767bc42b33b02cc16024918b7ecffd
Xiaofan Wu <xiaofan2406@gmail.com>
2021-09-24 16:50:56 +1000

    chore: add generator

---
+++ scripts/generator/main.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const {readFile, writeFile, appendFile, mkdir} = require('fs/promises');
+
+const pagePath = path.join(__dirname, '../../docs/pages');
+const srcPath = path.join(__dirname, '../../src');
+
+async function main(...args) {
+  if (args.length < 3) {
+    console.error('Require a component name');
+    process.exit(1);
+  }
+  let [, , [start, ...name]] = args;
+
+  const componentName = [start.toUpperCase(), ...name].join('');
+
+  const replaceAll = (data) => {
+    return data
+      .toString()
+      .replace(new RegExp('__Name__', 'g'), componentName)
+      .replace(new RegExp('__name__', 'g'), componentName.toLowerCase());
+  };
+
+  await mkdir(path.join(srcPath, componentName));
+
+  await Promise.all(
+    [
+      [
+        path.join(pagePath, `/${componentName}.page.tsx`),
+        path.join(__dirname, './page-template'),
+      ],
+      [
+        path.join(srcPath, `/${componentName}/${componentName}.css`),
+        path.join(__dirname, './css-template'),
+      ],
+      [
+        path.join(srcPath, `/${componentName}/${componentName}.tsx`),
+        path.join(__dirname, './src-template'),
+      ],
+      [
+        path.join(srcPath, `/${componentName}/${componentName}.spec.tsx`),
+        path.join(__dirname, './test-template'),
+      ],
+      [
+        path.join(srcPath, `/${componentName}/index.ts`),
+        path.join(__dirname, './index-template'),
+      ],
+      [path.join(srcPath, `/index.ts`)],
+    ].map(async ([filePath, templatePath]) => {
+      if (!templatePath) {
+        // src index
+        return appendFile(filePath, `export * from './${componentName}';\n`);
+      }
+      const fileContent = replaceAll(await readFile(templatePath));
+
+      return writeFile(filePath, fileContent);
+    })
+  );
+}
+
+main(...process.argv);
