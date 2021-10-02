8708db963285538b20e5c48baeacc6bf62acc129
Xiaofan Wu <xiaofan2406@gmail.com>
2021-10-02 22:33:05 +1000

    feat: add basic input (#6)

---
+++ docs/pages/Input.page.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import {Box, Input} from 'nidalee';
+
+const Demo1 = () => {
+  return <Input />;
+};
+
+const InputPage = () => {
+  return (
+    <>
+      <Demo1 />
+      <Box className="w-100 h-48 p-4" layer="base">
+        <Input />
+      </Box>
+    </>
+  );
+};
+
+export default InputPage;
+
+export const info = {
+  category: 'components',
+};
