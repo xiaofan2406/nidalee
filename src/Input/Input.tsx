a2ffe908fe8508b23d1a8e29f2865368b44414a4
Xiaofan Wu <xiaofan2406@gmail.com>
2021-10-16 14:50:16 +1100

    feat: add input field (#39)

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -4,9 +4,7 @@ import {cx} from '../utils';
 import './Input.css';
 
 export interface InputProps
-  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'children'> {
-  label?: React.ReactNode;
-}
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'children'> {}
 
 export const Input = (props: InputProps) => {
   const {className, ...rest} = props;
